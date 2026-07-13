e5702f5e965acca622ef39819483e856df4ce792
youbeenPark <qkrdbqls3286@gmail.com>
2026-07-13 19:11:01 +0900

    메인홈페이지 수정 2차

--- views/assets/js/sidebar.js
+++ views/assets/js/sidebar.js
@@ -1,23 +1,44 @@
+// const menuItems = document.querySelectorAll(".menu-item");
+
+// menuItems.forEach(item => {
+
+//     const toggle = item.querySelector(".menu-toggle");
+
+//     if (!toggle) return;
+
+//     toggle.addEventListener("click", () => {
+
+//         // 다른 메뉴는 닫기 (아코디언)
+//         menuItems.forEach(other => {
+//             if (other !== item) {
+//                 other.classList.remove("active");
+//             }
+//         });
+
+//         // 현재 메뉴 열기/닫기
+//         item.classList.toggle("active");
+
+//     });
+
+// });
+
 const menuItems = document.querySelectorAll(".menu-item");
 
 menuItems.forEach(item => {
-
     const toggle = item.querySelector(".menu-toggle");
 
     if (!toggle) return;
 
     toggle.addEventListener("click", () => {
 
-        // 다른 메뉴는 닫기 (아코디언)
-        menuItems.forEach(other => {
-            if (other !== item) {
-                other.classList.remove("active");
-            }
-        });
-
-        // 현재 메뉴 열기/닫기
         item.classList.toggle("active");
 
-    });
+        const arrow = toggle.querySelector(".arrow");
 
+        if (item.classList.contains("active")) {
+            arrow.textContent = "⌃";
+        } else {
+            arrow.textContent = "⌵";
+        }
+    });
 });
