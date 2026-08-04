771f6056dcc87d3183a612851f91cb1a67dcda4c
youbeenPark <qkrdbqls3286@gmail.com>
2026-08-04 11:25:49 +0900

    페이지 연결

--- routers/public.js
+++ routers/public.js
@@ -13,4 +13,36 @@ router.get('/equipment', (req, res) => {
 router.get('/equipment/status', (req, res) => {
     res.render('equipment/status');
 });
+
+router.get('/equipment/guide', (req, res) => {
+    res.render('equipment/guide');
+});
+
+router.get('/board/notice', (req, res) => {
+    res.render('board/notice');
+});
+
+router.get('/board/freeboard', (req, res) => {
+    res.render('board/freeboard');
+});
+
+router.get('/board/qna', (req, res) => {
+    res.render('board/qna');
+});
+
+router.get('/rental', (req, res) => {
+    res.render('rental/index');
+});
+
+router.get('/mypage/profile', (req, res) => {
+    res.render('mypage/profile');
+});
+
+router.get('/mypage/rental', (req, res) => {
+    res.render('mypage/rental');
+});
+
+router.get('/mypage/warning', (req, res) => {
+    res.render('mypage/warning');
+});
 module.exports = router;
